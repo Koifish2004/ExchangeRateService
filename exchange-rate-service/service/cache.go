5be0cf6ec3a325b29d726a35efb03ca6be5e2694
Kaif Ali Khan <kaifkhan@Mohammeds-MacBook-Air.local>
2025-11-26 00:23:44 +0530

    [FEATURE] added in memor caching

---
+++ exchange-rate-service/service/cache.go
@@ -0,0 +1,90 @@
+package service
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	latestRates map[string]float64
+	lastUpdated time.Time
+
+	historicalRates map[string]map[string]float64
+
+	mu sync.RWMutex
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		latestRates:     make(map[string]float64),
+		historicalRates: make(map[string]map[string]float64),
+	}
+}
+
+func (c *Cache) GetLatestRates() (map[string]float64, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	if len(c.latestRates) == 0 {
+		return nil, false
+	}
+
+	return c.latestRates, true
+
+}
+
+func (c *Cache) SetLatestRates(rates map[string]float64) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.latestRates = rates
+	c.lastUpdated = time.Now()
+}
+
+func (c *Cache) GetLastUpdated() time.Time {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	return c.lastUpdated
+}
+
+func (c *Cache) GetHistoricalRates(date time.Time) (map[string]float64, bool) {
+	dateKey := date.Format("2006-01-02")
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	rates, exists := c.historicalRates[dateKey]
+	if !exists {
+		return nil, false
+	}
+
+	return rates, true //check if latestrates can be changes externally
+
+}
+
+func (c *Cache) SetHistoricalRates(date time.Time, rates map[string]float64) {
+	dateKey := date.Format("2006-01-02")
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	c.historicalRates[dateKey] = rates
+
+}
+
+func (c *Cache) ClearOldHistoricalData() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	cutoffDate := time.Now().AddDate(0, 0, -90)
+
+	for dateStr := range c.historicalRates {
+		date, err := time.Parse("2006-01-02", dateStr)
+		if err != nil {
+			continue
+		}
+
+		if date.Before(cutoffDate) {
+			delete(c.historicalRates, dateStr)
+		}
+	}
+}
